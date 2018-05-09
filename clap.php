c1d9a66366f1318ecf510af0a77449d506bb581f
David Aragones <david@Mac-de-David.local>
2018-05-09 23:57:57 +0200

    és el primer commit cap a master

---
+++ clap.php
@@ -0,0 +1,70 @@
+
+<?php
+
+function readFileToArray($file) {
+
+	$result = array();
+
+	if (($handle = fopen($file, "r")) !== FALSE) {
+		$row = 0;
+	    while (($data = fgetcsv($handle, 10000, " ")) !== FALSE) {
+	        $num = count($data);
+	        if($row > 0) {
+	        	$result[] = $data;
+	        }
+	        $row++;
+	        /*
+	        for ($c=0; $c < $num; $c++) {
+	            echo $data[$c] . "<br />\n";
+	        }
+	        */
+	    }
+	    fclose($handle);
+	}
+
+	return $result;
+
+}
+
+
+function calculateFriends($S, $audience) {
+	$cntClappers = 0;
+	$cntAddedFriends = 0;
+
+	if ($S != (strlen($audience) - 1)) die("wrong input");
+
+	for($i=0; $i<strlen($audience); $i++) {
+		$shynessLevel = $i;
+		if ($shynessLevel == 0) {
+			$cntClappers = $audience[$shynessLevel];
+		}
+		else {
+			if ($cntClappers >= $shynessLevel) {
+				$cntClappers = $cntClappers + $audience[$shynessLevel];
+			}
+			else {
+				$newAddedFriends = ($shynessLevel - $cntClappers);
+				$cntAddedFriends += $newAddedFriends;
+				$cntClappers +=  $audience[$shynessLevel] + $newAddedFriends;
+			}
+		}
+	}
+	return $cntAddedFriends;
+}
+
+
+function main($inputFile) {
+	//echo calculateFriends(4, '12345') . PHP_EOL;
+	//echo calculateFriends(5, '110011') . PHP_EOL;
+	//echo $argv[1];
+	
+	$a = readFileToArray($inputFile);
+
+	foreach($a as $key=>$test) {
+		echo sprintf("Case #%d: %d%s", $key+1, calculateFriends($test[0], $test[1]), PHP_EOL);
+	}
+
+}
+
+main($argv[1]);
+?>
